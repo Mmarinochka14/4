bef38dc0dda77fb2d4a361d62610435dd95c8bfe
Mmarinochka14 <marinkilll14@gmail.com>
2024-01-06 22:03:00 +0300

    1 версия

---
+++ Testik/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+
+namespace Testik
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Test1 test1 = new Test1();
+            this.Hide();
+
+            test1.ShowDialog();
+            this.Show();
+        }
+
+    }
+}
